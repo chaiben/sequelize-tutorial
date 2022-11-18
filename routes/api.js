9a1d1a8368b89cc6596ef732157ec35f04fb0d10
Marçal Machado Chaiben <marcal.chaiben@qustodio.com>
2022-11-18 12:30:50 +0100

    Crea un API Rest con Express y MySQL (Parte IV)

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 
+const middleware = require('../routes/middlewares')
 const apiFilmsRouter = require('../routes/api/films')
 const apiUsersRouter = require('../routes/api/users')
 
-router.use('/films', apiFilmsRouter)
+router.use('/films', middleware.checkToken, apiFilmsRouter)
 router.use('/users', apiUsersRouter)
 
 module.exports = router
